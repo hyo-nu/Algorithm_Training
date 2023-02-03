f802fda480115a66b495faabf46c26b2ef320c04
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-02-03 17:15:51 +0900

    [D1] Title: 1대1 가위바위보, Time: 162 ms, Memory: 58,740 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -1,13 +1,17 @@
-A, B = map(int,input().split())
+A,B = map(int,input().split())
 
 if A == 1:
-    if B == 2:
+    if B == 1:
+        pass
+    elif B == 2:
         print('B')
     elif B == 3:
         print('A')
 elif A == 2:
     if B == 1:
         print('A')
+    elif B == 2:
+        pass
     elif B == 3:
         print('B')
 elif A == 3:
@@ -15,3 +19,5 @@ elif A == 3:
         print('B')
     elif B == 2:
         print('A')
+    elif B == 3:
+        pass
