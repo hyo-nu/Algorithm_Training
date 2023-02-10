49469b3b8954be402102a4641b62261dcf922450
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-02-10 20:41:37 +0900

    [D2] Title: 간단한 압축 풀기, Time: 161 ms, Memory: 58,508 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -1,19 +1,18 @@
 Test = int(input())
 
 for T in range(Test):
-    num = int(input())
-    next = 0
-
-    result = []
-    for n in range(num):
-        alph, cnt = input().split()
-        
-        for c in range(int(cnt)):
-            result.append(alph)
+    N = int(input())
+    words = ''
+    for i in range(N):
+        word,num = input().split()
+        words += word*int(num)
+    
+    jump = 0
     print(f'#{T+1}')
-
-    for r in range(1,len(result)+1):
+    for j in words:
+        jump += 1
+        print(j,end = '')
+        if jump % 10 == 0 : print()
+    print()    
         
-        if r % 10 == 0 : print(result[r-1] , end = '') ; print()
-        else : print(result[r-1] , end = '')
-    print()          
+    
