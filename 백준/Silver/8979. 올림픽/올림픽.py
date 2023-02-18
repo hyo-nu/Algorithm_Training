798ee5998f706ad61686c50c4822ff2d1f5c7931
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-02-19 00:02:46 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,20 +1,14 @@
-N,con = map(int,input().split())
-
+N,K = map(int,input().split())
 medal = [list(map(int,input().split())) for _ in range(N)]
-rank_dict = {}
 
-for c in range(1,N): # 1,2,3 / 금,은,동
-    max = 0
+medal_lst = sorted(medal,key = lambda x : (-x[1],-x[2],-x[3]))
+first = [medal_lst[0][1],medal_lst[0][2],medal_lst[0][3]]
+rank = 1
 
-    for r in range(N): # 0 1 2 3
-        if max < medal[r][c] : # 첫 메달 갯수 큰놈 찾기
-            max = medal[r][c]
-            r_idx = r # 그때 선수의 idx
-    rank_dict.setdefault(medal[r_idx][0],c) # 딕셔너리 c:등수, medal[][0] : 선수 저장
-
-    for j in range(N):
-        if j !=r_idx and max == medal[j][c]: # max랑 동점 비교
-            rank_dict.setdefault(medal[j][0],c) # 딕셔너리 {등수 : 선수} 이렇게 추가
-        medal[r_idx][j] = 0
-
-print(rank_dict[con])
+for num,G,S,B in medal_lst:
+    if first != [G,S,B]:
+        rank += 1
+    first = [G,S,B]
+    if num == K:
+        print(rank)
+        break
