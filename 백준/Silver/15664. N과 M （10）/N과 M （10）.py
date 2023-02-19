805d832746175a0e97bcd2f02655f94355e39524
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-02-20 00:00:21 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,18 @@
+# 현우 코드
+def DFS(n,lst):
+    if n >= N:
+        if len(lst) == M and lst not in ans :
+            ans.append(lst)
+        return
+
+    DFS(n + 1, lst + [nums[n]])
+    DFS(n + 1, lst)
+
+N,M = map(int,input().split())
+nums = list(map(int,input().split()))
+nums.sort()
+
+ans = []
+DFS(0,[])
+for lst in ans:
+    print(*lst)
