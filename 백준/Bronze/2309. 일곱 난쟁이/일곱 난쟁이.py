acb135ec999656798daea3218196a37e0b45cb49
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-02-22 20:01:04 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,17 @@
+def back(n,lst,sum):
+    if n > 8:
+        if len(lst) == 7 and sum == 100:
+            ans.append(lst)
+        return
+    if sum > 100 :
+        return
+    back(n+1,lst + [seven[n]],sum + seven[n])
+    back(n+1,lst,sum)
+
+seven = [int(input()) for _ in range(9)]
+ans = []
+back(0,[],0)
+result = sorted(ans[0])
+
+for i in result:
+    print(i)
