79a5226d0d01fd92d05c290ffb18a8864ec58bae
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-02-28 23:55:26 +0900

    [Bronze I] Title: 색종이, Time: 268 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,23 +1,24 @@
-
 N = int(input()) # N <= 10
 num = 0
 Map = [[0] * 1001 for _ in range(1001)]
 
 cnt = []
-R = C = 0
+rs = cs = re = ce = 0
 for n in range(N):
-    RS,CS,W,H = map(int,input().split())
+    R,C,W,H = map(int,input().split())
     num += 1
-    if R < RS + W : R = RS + W
-    if C < CS + H : C = CS + H
-    for r in range(RS,RS + W):
-        for c in range(CS,CS + H):
+    if re < R + W : re = R + W
+    if ce < C + H : ce = C + H
+    if rs > R : rs = R
+    if cs > C : cs = C
+    for r in range(R,R + W):
+        for c in range(C,C + H):
             Map[r][c] = num
 
 for i in range(1,N+1):
     cnt = 0
-    for r in range(R):
-        for c in range(C):
+    for r in range(rs,re):
+        for c in range(cs,ce):
             if Map[r][c] == i:
                 cnt += 1
     print(cnt)
