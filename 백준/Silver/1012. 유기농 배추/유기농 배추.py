42b0abd480bd12f5851954451fbd2870a005ac49
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-03-01 21:52:13 +0900

    [Silver II] Title: 유기농 배추, Time: 320 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,32 +1,35 @@
+
 from collections import deque
 
 def BFS():
+    Q = deque()
     dr = [0, 1, 0,-1] #우하좌상
     dc = [1, 0,-1, 0]
-    while Q:
-        r,c = Q.pop()
-        G[r][c] = 0
-        for d in range(4):
-            nr = r + dr[d]
-            nc = c + dc[d]
-            if 0 <= nr < N and 0<= nc < M and G[nr][nc] == 1:
-                Q.append((nr, nc))
+    result = 0
+    for x in range(N):
+        for y in range(M):
+            if G[x][y] == 1:
+                Q.append((x,y))
+                G[x][y] = 0
+                result += 1
+                while Q:
+                    r,c = Q.popleft()
+                    for d in range(4):
+                        nr = r + dr[d]
+                        nc = c + dc[d]
+                        if 0 <= nr < N and 0<= nc < M and G[nr][nc] == 1:
+                            Q.append((nr, nc))
+                            G[nr][nc] = 0
+    return result
 
 Test = int(input())
 
 for TC in range(Test):
     M, N, K = map(int,input().split())
     G = [[0] * (M) for _ in range(N)]
-    Q = deque()
-    result = 0
+
     for _ in range(K):
         y,x = map(int,input().split())
         G[x][y] = 1
 
-    for r in range(N):
-        for c in range(M):
-            if G[r][c] == 1:
-                Q.append((r,c))
-                BFS()
-                result += 1
-    print(result)
+    print(BFS())
