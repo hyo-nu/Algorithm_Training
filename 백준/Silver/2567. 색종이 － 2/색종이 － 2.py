10703b2df344d393dd0c47afef6a3424a670f708
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-03-04 00:27:52 +0900

    [Silver IV] Title: 색종이 - 2, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -0,0 +1,29 @@
+paper = int(input())
+G = [[0] * 102 for _ in range(102)]
+
+for _ in range(paper):
+    X,Y = map(int,input().split())
+    for r in range(1,11):
+        for c in range(1,11):
+            G[X+r][Y+c] = 1
+
+dr = [-1, 1, 0, 0] # 상하 좌우
+dc = [ 0, 0,-1, 1]
+Sum = 0
+
+for r in range(102):
+    for c in range(102):
+        if G[r][c] == 1:
+            for d in range(2): # 상하
+                nr = r + dr[d]
+                nc = c + dc[d]
+                if G[nr][nc] == 0:
+                    Sum += 1
+                    break
+            for d in range(2,4): # 좌우
+                nr = r + dr[d]
+                nc = c + dc[d]
+                if G[nr][nc] == 0:
+                    Sum += 1
+                    break
+print(Sum)
