02c510d8617e51e752a6ce47f464c4be70d9f577
jeonghyunwooo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-03-03 09:35:01 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40292 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,10 @@
+N, K = map(int,input().split())
+temp = list(map(int,input().split()))
+
+
+Sum = sum([temp[i] for i in range(K)])
+Max = Sum
+for i in range(N-K): # 0 1 2 3 4 5 6 7
+    Sum = Sum - temp[i] + temp[i+K]
+    if Max < Sum : Max = Sum
+print(Max)
