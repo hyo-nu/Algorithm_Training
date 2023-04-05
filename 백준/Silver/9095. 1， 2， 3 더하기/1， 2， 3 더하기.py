f990c1e4094f362985ddc8afa18ea16f54b68455
Hyun woo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-04-05 17:14:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,16 @@
+def back(sm):
+    global cnt
+    if sm > N : return
+    elif sm == N :
+        cnt += 1
+        return
+    back(sm+1)
+    back(sm+2)
+    back(sm+3)
+
+Test = int(input())
+for TC in range(Test):
+    cnt = 0
+    N = int(input())
+    back(0)
+    print(cnt)
