6ce75b3b07717475ea49dbc598333115dbba6ac1
Hyun woo <122512457+jeonghyunwooo@users.noreply.github.com>
2023-05-05 19:51:57 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 256 ms, Memory: 57436 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,11 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 N,M = map(int,input().split())
 site_dict = {}
 for _ in range(N):
     site,password = input().split()
     site_dict.setdefault(site,password)
 
-find_pass = [input() for _ in range(M)]
+find_pass = [input().rstrip() for _ in range(M)]
 
 for find in find_pass:
     print(site_dict[find])
-
