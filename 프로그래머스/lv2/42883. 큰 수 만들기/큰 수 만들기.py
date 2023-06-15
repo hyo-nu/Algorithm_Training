d3e9591b4772b5902ea09a47712907d6cb033ccc
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-06-15 13:02:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,20 +1,10 @@
-# def solution(number, k):
-#     answer = ''
-#     remove = 0
-#     for i in range(len(number)):
-#         while k > remove and answer and answer[-1] < number[i] :
-#             answer = answer[:-1]
-#             remove += 1
-#         answer += number[i]
-#     return answer
-
 def solution(number, k):
-    answer = [] # Stack
-    
-    for num in number:
-        while k > 0 and answer and answer[-1] < num:
-            answer.pop()
-            k -= 1
-        answer.append(num)
-        
-    return ''.join(answer[:len(answer) - k])
+    answer = ''
+    remove = 0
+    for i in range(len(number)):
+        while k > remove and answer and answer[-1] < number[i] :
+            answer = answer[:-1]
+            remove += 1
+        answer += number[i]
+    return answer[:len(answer)-(k-remove)]
+
