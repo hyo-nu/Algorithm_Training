461ab724943ebb7276705ef699cc3f9e1803a378
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-06-16 10:54:54 +0900

    [Silver II] Title: 이동하기, Time: 816 ms, Memory: 70108 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,8 @@
+N, M = map(int,input().split())
+dp = [[0] * (M + 1)] + [[0] + list(map(int,input().split())) for _ in range(N)]
+
+for r in range(1,N+1):
+    for c in range(1,M+1):
+        dp[r][c] = dp[r][c] + max(dp[r-1][c],dp[r][c-1],dp[r-1][c-1])
+
+print(dp[N][M])
