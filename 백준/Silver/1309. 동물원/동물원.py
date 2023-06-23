6c45c881b515b64e6e83861f632560c04ff3f96e
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-06-23 12:24:37 +0900

    [Silver I] Title: 동물원, Time: 800 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,8 +1,5 @@
 N = int(input())
-dp = [1,1,1]
-for _ in range(N-1):
-    for i in range(3):
-        if i == 0 : dp[i] = sum(dp)
-        else: dp[i] = dp[0] - dp[i]
-            
-print(sum(dp) % 9901)
+n0 = n1 = n2 = 1
+for n in range(1, N):
+    n0, n1, n2 = (n0 + n1 + n2), (n0 + n1), (n0 + n1)
+print((n0 + n1 + n2) % 9901)
