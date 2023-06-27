ba7e8207310187ecd762b759fe1153c44949a940
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-06-28 01:14:23 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,19 +1,16 @@
 from collections import deque
 
 def BFS():
-    move = [1,-1]
     while Q:
         XB = Q.popleft()
-        for d in range(2):
-            XA = XB * 2
-            if 0 <= XA <= 100000 and visited[XA] == -1:
-                Q.appendleft(XA)
-                visited[XA] = visited[XB]
-            XA = XB + move[d]
+        if 0 <= XB * 2 <= 100000 and visited[XB * 2] == -1:
+            Q.appendleft(XB * 2)
+            visited[XB * 2] = visited[XB]
+        for d in (-1,1):
+            XA = XB + d
             if 0 <= XA <= 100000 and visited[XA] == -1:
                 Q.append(XA)
                 visited[XA] = visited[XB] + 1
-
             if XA == K :
                 return
 
