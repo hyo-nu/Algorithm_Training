bc3e98c5f3655c8052447d59bc081d693fc4e1dc
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-07-01 17:09:22 +0900

    [Gold IV] Title: 트리의 지름, Time: 480 ms, Memory: 34488 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,29 +1,29 @@
 from collections import deque
 
 def BFS(sp):
-    Q = deque()
-    Q.append(sp)
-    vi = [-1] * (N + 1)
-    vi[sp] = longest = longest_point = 0
+    Q = deque([sp])
+    vi = [-1] * (Node + 1)
+    vi[sp] = long = ep = 0
     while Q:
         sp = Q.popleft()
-        for np, w in G[sp]:
+        for np, weight in G[sp]:
             if vi[np] == -1:
-                vi[np] = vi[sp] + w
+                vi[np] = vi[sp] + weight
                 Q.append(np)
-                if longest < vi[np]:
-                    longest = vi[np]
-                    longest_point = np
-    return longest, longest_point
+                if long < vi[np]:
+                    long = vi[np]
+                    ep = np
+    return long, ep
 
-N = int(input())
-G = [[] for _ in range(N+1)]
-for _ in range(N-1):
-    sp, ep, w = map(int,input().split())
-    G[sp].append((ep,w))
-    G[ep].append((sp,w))
+Node = int(input())
+G = [[] for _ in range(Node+1)]
+
+for _ in range(Node-1):
+    u, v, weight = map(int,input().split())
+    G[u].append((v,weight))
+    G[v].append((u,weight))
 
 start = 1
-tmp1, end = BFS(start)
-diameter, tmp2 = BFS(end)
+tmp, end = BFS(start)
+diameter, tmp = BFS(end)
 print(diameter)
