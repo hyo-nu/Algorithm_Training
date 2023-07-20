7dc91b93c859c45d2a3b2d6db2249b0adea057fe
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-07-20 22:59:46 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
@@ -1,4 +1,3 @@
-score = list(map(int,list(input())))
-half = len(score)//2
-left, right = score[:half], score[half:]
-print("LUCKY") if sum(left) == sum(right) else print("READY")
+N = list(map(int,list(input())))
+print("LUCKY") if sum(N[:len(N)//2]) == sum(N[len(N)//2:]) else print("READY")
+
