b74e3526c35824adc59b9bcce8d6438dfcf5f94b
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-07-23 15:11:51 +0900

    [Gold IV] Title: 뱀, Time: 72 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,52 +1,51 @@
 from collections import deque
 
+# N 크기 만큼 G 만들기
 N = int(input())
 G = [[0] * N for _ in range(N)]
 
+# 사과의 위치 표시하기
 for _ in range(int(input())):
     r, c = map(int,input().split())
     G[r-1][c-1] = 1
 
-turn = [list(map(str,input().split())) for _ in range(int(input()))]
+def snakeMove():
+    global time, stop, d
+    r, c = snake[-1][0], snake[-1][1] # 현재 뱀 머리 위치
+    nr, nc = r + dr[d], c + dc[d]     # 다음 뱀 머리 위치
+    snake.append((nr, nc))            # 다음 뱀 머리 위치 담기
+    G[r][c] = 2                       # 현재 뱀 꼬리 표시
+    time += 1                         # 1초 으흠
 
-dr = [0, 1, 0, -1] # 우 하 좌 상
-dc = [1, 0, -1, 0]
-snake = deque([(0,0)])
-turn = deque(turn)
-time = d = 0
+    # 뱀이 벽 or 자기 몸에 부딫히면 stop
+    if nr < 0 or nr >= N or nc < 0 or nc >= N or G[nr][nc] == 2:
+        stop = 1
+        return
 
-while turn:
-    move, dire = turn.popleft()
+    # 이동 위치에 사과가 없을 때
+    if G[nr][nc] == 0:
+        br, bc = snake.popleft() # 꼬리를 잘라서
+        G[br][bc] = 0            # G에는 0으로 표시한다.
+
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]     # 우 하 좌 상
+snake = deque([(0,0)]) # 뱀 시작위치
+time = stop = d = 0    # 총 시간, 뱀 방향
+
+for _ in range(int(input())):
+    move, dire = map(str,input().split())
     move = int(move) - time
     for _ in range(move):
-        r, c = snake[-1][0], snake[-1][1] # 이전 뱀 머리 위치
-        nr, nc = r + dr[d], c + dc[d]     # 현재 뱀 머리 위치
-        snake.append((nr,nc))
-        G[r][c] = 2
-        time += 1
-        if nr < 0 or nr >= N or nc < 0 or nc >= N or G[nr][nc] == 2:
-            break
-        # 사과가 없을 때
-        if G[nr][nc] == 0 :
-            br, bc = snake.popleft()
-            G[br][bc] = 0
+        snakeMove()
+        if stop == 1: break
+    if stop == 1 : break
 
+    # 방향 전환
     if dire == "D" : d = ( d + 1 ) % 4
     else : d = ( d + 3 ) % 4
-    if nr < 0 or nr >= N or nc < 0 or nc >= N or G[nr][nc] == 2:
-        break
 
-while not turn:
-    if nr < 0 or nr >= N or nc < 0 or nc >= N or G[nr][nc] == 2:
-        break
-    r, c = snake[-1][0], snake[-1][1]  # 이전 뱀 머리 위치
-    nr, nc = r + dr[d], c + dc[d]  # 현재 뱀 머리 위치
-    snake.append((nr, nc))
-    G[r][c] = 2
-    time += 1
-    # 사과가 없을 때
-    if G[nr][nc] == 0:
-        br, bc = snake.popleft()
-        G[br][bc] = 0
+# 방향 전환 정보를 다 체크했는데도 게임이 안 끝났을 때
+while stop == 0:
+    snakeMove()
 
 print(time)
