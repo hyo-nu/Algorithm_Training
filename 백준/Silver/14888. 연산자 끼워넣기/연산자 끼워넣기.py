4ef830247e0f5697f81d409d9bf1749cec6ed711
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-08-28 23:50:34 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,29 +1,24 @@
-def back(n,Sum):
-    global Min,Max
-    if n == N:
-        Min = min(Min,Sum)
-        Max = max(Max,Sum)
+a=int(input())
+b=list(map(int, input().split()))
+add, sub, mul, div=map(int, input().split())
+maxi,mini=-1000000000,1000000000
+ans=0
+def cal(add, sub, mul, div, ans, i):
+    global maxi, mini
+    if i==a:
+        if ans>=maxi:
+            maxi=ans
+        if ans<=mini:
+            mini=ans
         return
-    for i in range(len(calcul)):
-        if not vi[i]:
-            vi[i] = 1
-            if calcul[i] == 0: back(n+1,Sum + nums[n])
-            if calcul[i] == 1: back(n+1,Sum - nums[n])
-            if calcul[i] == 2: back(n+1,Sum * nums[n])
-            if calcul[i] == 3:
-                if Sum >= 0 : back(n+1,Sum // nums[n])
-                else : back(n + 1, -(abs(Sum) // nums[n]))
-            vi[i] = 0
-
-N = int(input())
-nums = list(map(int,input().split()))
-signs = list(map(int,input().split()))
-calcul = []
-for idx, sign in enumerate(signs):
-    tmp = [idx] * sign
-    calcul += tmp
-Max, Min = -1000000001, 1000000001
-vi = [0] * len(calcul)
-back(1,nums[0])
-print(Max)
-print(Min)
+    if add>0:
+        cal(add-1, sub, mul, div, ans+b[i], i+1)
+    if sub>0:
+        cal(add, sub-1, mul, div, ans-b[i], i+1)
+    if mul>0:
+        cal(add, sub, mul-1, div, ans*b[i], i+1)
+    if div>0:
+        cal(add, sub, mul, div-1, int(ans/b[i]), i+1)
+cal(add, sub, mul, div, b[0], 1)
+print(maxi)
+print(mini)
