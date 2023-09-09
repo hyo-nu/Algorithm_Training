47e88e4ad81bfb05facbbd2a18575eeb633d31cc
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-09-09 18:43:45 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 40548 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N, C = map(int,input().split())
+wifi = sorted([int(input()) for _ in range(N)])
+
+def binary_search(array, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        current = array[0]
+        count = 1
+
+        for i in range(1, len(array)):
+            if array[i] >= current + mid:
+                count += 1
+                current = array[i]
+
+        if count >= C:
+            global answer
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+
+start = 1
+end = wifi[-1] - wifi[0]
+answer = 0
+binary_search(wifi,start,end)
+print(answer)
