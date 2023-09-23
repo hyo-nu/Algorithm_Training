fad24ce0a4385ca0d698b9d70fe3fb2c2c57bc14
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-09-23 12:22:33 +0900

    [Gold IV] Title: 키 순서, Time: 1004 ms, Memory: 35384 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.py
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -1,28 +1,38 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-INF = int(1e9)
+def BFS(start, arr):
+    global order
+    Q = deque()
+    Q.append(start)
+    vi = [0] * (N + 1)
+    vi[start] = 1
+
+    while Q:
+        sp = Q.popleft()
+        for np in arr[sp]:
+            if not vi[np]:
+                Q.append(np)
+                vi[np] = 1
+                order += 1
+
 N, M = map(int,input().split())
-G = [[INF] * (N + 1) for _ in range(N + 1)]
+G = [[] for _ in range(N + 1)]     
+G_re = [[] for _ in range(N + 1)]  
 
 for _ in range(M):
-    u, v = map(int,input().split())
-    G[u][v] = 1
-
-for i in range(1, N + 1):
-    G[i][i] = 0
-
-for k in range(1, N + 1):
-    for r in range(1, N + 1):
-        for c in range(1, N + 1):
-            G[r][c] = min(G[r][c], G[r][k] + G[k][c])
+    A,B = map(int,input().split())
+    G[A].append(B)     # 학생 기준으로 점수 높은 친구들 확인용
+    G_re[B].append(A)  # 학생 기준으로 점수 낮은 친구들 확인용
 
 student = 0
-for r in range(1, N + 1):
-    count = 0
-    for c in range(1, N + 1):
-         if (G[r][c] != INF and G[r][c] != 0) or (G[c][r] != INF and G[c][r] != 0):
-            count += 1
-    if count == N - 1 : student += 1
+for start in range(1,N + 1):  
+    order = 0
+    BFS(start,G)       # start라는 학생 기준으로 높은 애들 몇명인지
+    BFS(start,G_re)    # start라는 학생 기준으로 낮은 애들 몇명인지
+
+		# 그 수가 자신을 제외한 숫자가 동일 하다면 내 순위 확인이 가능
+    if order == N-1 : student += 1  
 
 print(student)
