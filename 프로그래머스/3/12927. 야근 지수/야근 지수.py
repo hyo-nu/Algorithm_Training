444dce095a2ced4a04bf170744a8dae7ffe5e3b8
Hyunwoo jeong <122512457+jeonghyunwooo@users.noreply.github.com>
2023-10-14 18:40:06 +0900

    [level 3] Title: 야근 지수, Time: 4.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,33 @@
+
+def solution(n, works):
+    works.sort(reverse = True)
+    works_len = len(works)
+    
+    for idx in range(1,works_len):
+        if works[idx-1] != works[idx]:
+            hour = (works[idx-1] - works[idx]) * idx
+            print(hour,idx)
+            if hour <= n :
+                n -= hour
+                for i in range(idx, 0, -1):
+                    works[i-1] = works[i]
+            
+            else:
+                hour = n // idx
+                remain = n % idx
+                
+                for i in range(idx-1, -1, -1):
+                    works[i] -= hour
+                    if i < remain : works[i] -= 1
+                break
+    else:
+        hour = n // works_len
+        remain = n % works_len
+        for i in range(works_len-1, -1, -1):
+            
+            works[i] -= hour
+            if i < remain : works[i] -= 1
+    
+    result = sum([works[i]**2 if works[i] > 0 else 0 for i in range(works_len)])
+    
+    return result
