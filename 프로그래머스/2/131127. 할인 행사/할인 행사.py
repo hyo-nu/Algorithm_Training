fc095829a6f1dcf3a23ff5ead02209fefe546418
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-21 16:46:37 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,19 +1,14 @@
 # 매일 한 가지 제품 1개 할인
 # 10일 연속할인 가능한 날에 회원가입
 # 회원 가입 가능한 일수 구하기
+from collections import Counter
 
 def solution(want, number, discount):
     shopping = {want[i] : number[i] for i in range(len(want))}
-    
     result = 0
-    for day in range(len(discount) - 9):
-        salse = {}
-        for i in range(10):
-            if discount[day + i] not in salse : salse[discount[day + i]] = 1
-            else : salse[discount[day + i]] += 1
-        for item, cnt in salse.items():
-            if item not in shopping: break
-            elif cnt > shopping[item] : break
-        else:
-            result += 1
+    
+    for i in range(len(discount) - 9):
+        if shopping == Counter(discount[i : i + 10]):
+              result += 1
+    
     return result
