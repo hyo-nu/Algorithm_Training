f8c76e542af119ccea8bd97dd8ba48a37827cc2d
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-21 14:43:32 +0900

    [Gold IV] Title: 연구소, Time: 1356 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,38 +1,40 @@
+import sys
 from collections import deque
 from itertools import combinations
-from copy import deepcopy
+input = sys.stdin.readline
 
-def BFS(arr,lst):
-    safe = 0
-    q = deque(lst)
-    while q:
-        r, c = q.popleft()
-        for dr, dc in ((0,1),(1,0),(0,-1),(-1,0)):
+def BFS(virus, unsafe):
+    Q = deque(virus)
+    area = unsafe
+    vi = [[1 if G[n][m] == 1 else 0 for m in range(M)] for n in range(N)]
+    for r, c in walls: vi[r][c] = 1
+    for r, c in Q : vi[r][c] = 2
+
+    while Q:
+        r, c = Q.popleft()
+        for dr, dc in ((0, 1),(1, 0),(0, -1),(-1, 0)):
             nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0:
-                q.append((nr,nc))
-                arr[nr][nc] = 1
+            if 0 <= nr < N and 0 <= nc < M and not vi[nr][nc]:
+                Q.append((nr,nc))
+                vi[nr][nc] = 1
+                area += 1
+                if Min <= area : return N * M
 
-    for lst in arr:
-        safe += lst.count(0)
-    return safe
+    return area
 
 N, M = map(int,input().split())
-G = []
-Q = deque()
-empty = []
+virus, roads, G = [], [], []
+unsafe, Min = 3, N * M
+
 for r in range(N):
     tmp = list(map(int,input().split()))
     G.append(tmp)
     for c in range(M):
-        if tmp[c] == 2 : Q.append((r,c))
-        if tmp[c] == 0 : empty.append((r,c))
+        if tmp[c] == 2 : virus.append((r,c)) ; unsafe += 1
+        elif tmp[c] == 0 : roads.append((r,c)) ;
+        else : unsafe += 1
 
-Max = 0
-for lst in combinations(empty, 3):
-    G_copy = deepcopy(G)
-    for R, C in lst:
-        G_copy[R][C] = 1
-    Max = max(Max,BFS(G_copy,Q))
+for walls in combinations(roads,3):
+    Min = min(Min,BFS(virus, unsafe))
 
-print(Max)
+print(N*M - Min)
