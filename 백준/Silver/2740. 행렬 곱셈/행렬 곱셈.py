ef5acb250992381f9c2dd908673c28aa78da6eda
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-22 14:02:16 +0900

    [Silver V] Title: 행렬 곱셈, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -7,7 +7,6 @@ arr1 = [list(map(int,input().split())) for _ in range(N)]
 M, K = map(int,input().split())
 arr2 = [list(map(int,input().split())) for _ in range(M)]
 
-result = [[sum([a * b for a, b in zip(lst1,lst2)]) for lst2 in list(zip(*arr2)) ] for lst1 in arr1]
 
-for lst in result:
+for lst in [[sum([a * b for a, b in zip(lst1,lst2)]) for lst2 in list(zip(*arr2)) ] for lst1 in arr1]:
     print(*lst)
