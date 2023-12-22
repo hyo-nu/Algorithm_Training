4b38c074756b8353a655817c3a3b3136ec23ac19
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-22 13:48:09 +0900

    [level 2] Title: 행렬의 곱셈, Time: 4.55 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,7 +1,28 @@
-import numpy as np
+# 3*2 2*3
+# 3*3 3*3 
 
 def solution(arr1, arr2):
-    arr1 = np.array(arr1)
-    arr2 = np.array(arr2)
-    result = (arr1 @ arr2).tolist()
-    return result
+    # print(*arr1)
+    # print(list(zip(*arr1)))    
+    # for lst in arr1:
+    #     print(lst)
+    # print("======")
+    # for lst in  list(zip(*arr2)):
+    #     print(lst)
+    
+    arr1 = arr1
+    arr2 = list(zip(*arr2))
+    arr = []
+    for lst1 in arr1:
+        value = []
+        for lst2 in arr2:
+            value.append(sum([a * b for a, b in zip(lst1,lst2)]))
+        arr.append(value)
+    return arr
+    
+# import numpy as np
+
+# def solution(arr1, arr2):
+#     arr1 = np.array(arr1)
+#     arr2 = np.array(arr2)
+#     return (arr1 @ arr2).tolist()
