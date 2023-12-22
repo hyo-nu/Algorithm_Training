496ba3f8cf04eaebebb112583fe47adb8c23d5ff
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-22 13:57:26 +0900

    [Silver V] Title: 행렬 곱셈, Time: 88 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -1,17 +1,13 @@
-N, MM = map(int,input().split())
-A = [list(map(int,input().split())) for _ in range(N)]
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+arr1 = [list(map(int,input().split())) for _ in range(N)]
 
 M, K = map(int,input().split())
-B = [list(map(int,input().split())) for _ in range(M)]
+arr2 = [list(map(int,input().split())) for _ in range(M)]
 
-result = []
-for a in A:
-    for k in range(K): # 0 1 2
-        sum = 0
-        for m in range(M): # 0 1
-            sum += a[m]*B[m][k]
-        result.append(sum)
+result = [[sum([a * b for a, b in zip(lst1,lst2)]) for lst2 in list(zip(*arr2)) ] for lst1 in arr1]
 
-for i in range(len(result)):
-    print(result[i],end = ' ')
-    if (i+1) % K == 0:print()
+for lst in result:
+    print(*lst)
