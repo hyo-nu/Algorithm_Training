84a59c18dcec5a44eee524226b947329de0ebfc8
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-24 17:32:47 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.js
@@ -2,11 +2,5 @@
 // 0 1 2 3 4 5 6 7 8 9
 
 function solution(age) {
-    const alpha = ["a","b","c","d","e","f","g","h","i","j"]
-    const numbers = String(age).split("")
-    const newage = numbers.map((number) => {
-        return alpha[Number(number)]
-    })
-    
-    return newage.join("")
+    return age.toString().split("").map((number) => {return "abcdefghij"[number]}).join("")
 }
