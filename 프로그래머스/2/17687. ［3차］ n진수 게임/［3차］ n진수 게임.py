e18b2cf2f63f8dd60a78a7173043ff0e4718752f
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-27 20:26:54 +0900

    [level 2] Title: [3차] n진수 게임, Time: 23.71 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,7 +1,7 @@
 def solution(n, t, m, p):
     code = {10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
     
-    def Decimal(num, n):
+    def n_decimal(num, n):
         value = ""
         while num:
             value += code.get(num % n,str(num % n))
@@ -10,7 +10,7 @@ def solution(n, t, m, p):
     
     total = "0"
     for i in range(t * m):
-        total += Decimal(i, n)
+        total += n_decimal(i, n)
         
     result = ""
     for i in range(t):
