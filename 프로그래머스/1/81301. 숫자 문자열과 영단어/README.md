fd7fd55e77aff688e6813b2ba897ecbdeeb0bb0a
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-28 20:31:42 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.08 ms
+메모리: 33.5 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 20:26:22
+2023년 12월 4일 20:31:40
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,24 +1,11 @@
 function solution(s) {
-    const nums = {
-        "zero" : "0",
-        "one" : "1",
-        "two" : "2",
-        "three" : "3",
-        "four" :"4",
-        "five" : "5",
-        "six" : "6",
-        "seven" : "7",
-        "eight" : "8",
-        "nine" : "9"
-    }
+    const nums =  ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
     let number = "", alpha = ""
     s.split("").forEach((val) => {
-        isNaN(Number(val)) ? alpha += val : number += val 
-        if (alpha in nums) { 
-            number += nums[alpha]
-            alpha = ""
-        }
+        isNaN(Number(val)) ? alpha += val : number += val
+        idx = nums.indexOf(alpha)
+        idx !== -1 ? (number += String(idx),alpha = "") : ("")
     })
     return Number(number)
 }
