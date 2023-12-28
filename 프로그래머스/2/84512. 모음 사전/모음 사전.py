448ec8ebed179d2db6e9826c906962484563ed9b
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-28 23:04:15 +0900

    [level 2] Title: 모음 사전, Time: 0.83 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,23 +1,36 @@
+from itertools import product
+
+# solution = lambda word: sorted(["".join(c) for i in range(5) for c in product("AEIOU", repeat=i+1)]).index(word) + 1
+
 def solution(word):
-    dictionary = {"0":"", "1":"A", "2":"E", "3":"I", "4":"O", "5":"U"}
     
-    def conversion (nums):
-        alpha = ""
-        for num in nums : alpha += dictionary[num]
-        return alpha 
-    nums, order = "", 0
-    while True:
+    order = 0
+    for lst in sorted(["".join(c) for i in range(5) for c in product("AEIOU", repeat = i + 1)]):
         order += 1
-        if len(nums) < 5 : nums += "1"
-        else : 
-            nums = list(str(int(nums) + 1))
-            while "6" in nums:
-                for i in range(5):
-                    if nums[i] == "6":
-                        nums[i] = "0"
-                        nums[i-1] = str(int(nums[i-1]) + 1)
-                        break
-            nums = "".join(nums)
-        if conversion(nums) == word:break
-        nums = nums[:5 - nums.count("0")]
+        if lst == word: break
     return order
+
+
+# def solution(word):
+#     dictionary = {"0":"", "1":"A", "2":"E", "3":"I", "4":"O", "5":"U"}
+    
+#     def conversion (nums):
+#         alpha = ""
+#         for num in nums : alpha += dictionary[num]
+#         return alpha 
+#     nums, order = "", 0
+#     while True:
+#         order += 1
+#         if len(nums) < 5 : nums += "1"
+#         else : 
+#             nums = list(str(int(nums) + 1))
+#             while "6" in nums:
+#                 for i in range(5):
+#                     if nums[i] == "6":
+#                         nums[i] = "0"
+#                         nums[i-1] = str(int(nums[i-1]) + 1)
+#                         break
+#             nums = "".join(nums)
+#         if conversion(nums) == word:break
+#         nums = nums[:5 - nums.count("0")]
+#     return order
