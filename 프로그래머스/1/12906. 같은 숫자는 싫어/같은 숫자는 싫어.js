d2bea3f8afbc7ad6bb54b8a166a6d9b4f4a80679
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-28 19:18:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.72 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,34 @@
+function solution(arr){
+    arr2 = []
+    
+    let same = -1
+    for (let i = 0; i < arr.length; i++){
+        if (same !== arr[i]){
+            arr2.push(arr[i])
+            same = arr[i]
+        }
+    }
+    return arr2
+}
+
+
+
+
+
+
+
+
+
+
+
+// function solution(arr)
+// {
+//     let ans = []
+    
+//     for (let i = 0; i < arr.length; i++ ){
+//         if (arr[i] !== ans[ans.length-1] ) {
+//             ans.push(arr[i])
+//         }
+//     }
+//     return ans;
+// }
