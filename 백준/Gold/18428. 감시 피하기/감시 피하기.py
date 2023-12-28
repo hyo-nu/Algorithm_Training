d02e520c8e198003603a68aeb7a4671dbdbe5fa7
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-28 21:01:07 +0900

    [Gold V] Title: 감시 피하기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,6 +1,21 @@
-from itertools import combinations
 import sys
 from itertools import combinations
+input = sys.stdin.readline
+
+def isMeet():
+    global meet
+    for r, c in T:
+        for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+            for n in range(1, N):  # 각 방향으로 한칸 씩 이동
+                nr, nc = r + dr * n, c + dc * n
+                if 0 <= nr < N and 0 <= nc < N:  # 그래프 범위 안에 있을 때만
+                    if G[nr][nc] == "O":  # 장애물 만나면
+                        break  # 해당 방향 탐색 끝
+                    elif G[nr][nc] == "S":  # 학생 만나면
+                        return True
+
+    meet = False
+    return False
 
 N = int(input())
 G, X, T = [], [], []
@@ -8,31 +23,17 @@ for R in range(N):
     tmp = list(map(str,input().split()))
     G.append(tmp)
     for C in range(N):
-        if tmp[C] == "X": X.append((R,C))
-        elif tmp[C] == "T": T.append((R,C))
+        if tmp[C] == "X" : X.append((R,C))
+        elif tmp[C] == "T" : T.append((R,C))
 
+meet = True
 for blocks in combinations(X,3):
     for r, c in blocks:
         G[r][c] = "O"
 
-    meet = 0
-    for r, c in T:
-        for dr, dc in ((-1,0),(0,1),(1,0),(0,-1)):
-            for n in range(1,N):
-                nr, nc = r + dr * n, c + dc * n
-                if 0 <= nr < N and 0 <= nc < N:
-                    if G[nr][nc] == "O":
-                        break
-                    elif G[nr][nc] == "S":
-                        meet += 1
-                        break
-            if meet : break
-        if meet: break
-    if not meet :
-        print("YES")
-        break
+    if not isMeet() : break
 
     for r, c in blocks:
         G[r][c] = "X"
 
-if meet : print("NO")
+print("NO") if meet else print("YES")
