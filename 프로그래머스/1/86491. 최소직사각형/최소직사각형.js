7b3f1bb6bf7c43363196a81c529d86a6859a1b6e
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-28 19:43:26 +0900

    [level 1] Title: 최소직사각형, Time: 6.16 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,16 +1,31 @@
 function solution(sizes) {
-    const sizes_sort = sizes.map((size,index) => {
-        if (size[0] >= size[1]){return [size[0], size[1]]}
-        else if (size[0] < size[1]){return [size[1], size[0]]}
-    })
-    let lmax = 0, rmax = 0
+    const sizes_sort = sizes.map(([w,h]) => w > h ? [w,h] : [h,w])
     
-    for(let size of sizes_sort){
-        if (lmax < size[0]) { lmax = size[0]}
-        if (rmax < size[1]) { rmax = size[1]}
-    }
-    return lmax * rmax
+    let wmax = 0, hmax  = 0
+    sizes_sort.forEach(([w, h]) => {
+        wmax = Math.max(wmax,w)
+        hmax = Math.max(hmax,h)
+    })
+    return wmax * hmax
 }
+
+// function solution(sizes) {
+// }
+
+// function solution(sizes) {
+//     const sizes_sort = sizes.map((size,index) => {
+//         if (size[0] >= size[1]){return [size[0], size[1]]}
+//         else if (size[0] < size[1]){return [size[1], size[0]]}
+//     })
+//     let lmax = 0, rmax = 0
+    
+//     for(let size of sizes_sort){
+//         if (lmax < size[0]) { lmax = size[0]}
+//         if (rmax < size[1]) { rmax = size[1]}
+//     }
+//     return lmax * rmax
+// }
+
 // function solution(sizes) {
 //     let ans = []
 //     let max_w = 0
