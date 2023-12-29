2ffe84656394581d85e49c40130ae586e3ad6036
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2023-12-29 10:54:51 +0900

    [Silver IV] Title: 덱, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,17 +1,24 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
-lst = deque()
+deque = []
 for _ in range(int(input())):
     value = input().split()
 
-    if value[0] == "push_front" : lst.appendleft(value[1])
-    elif value[0] == "push_back": lst.append(value[1])
-    elif value[0] == "pop_front": print(lst.popleft()) if lst else print(-1)
-    elif value[0] == "pop_back": print(lst.pop()) if lst else print(-1)
-    elif value[0] == "front": print(lst[0]) if lst else print(-1)
-    elif value[0] == "back": print(lst[-1]) if lst else print(-1)
-    elif value[0] == "size": print(len(lst))
-    elif value[0] == "empty": print(0) if lst else print(1)
+    if value[0] == "push_front" :
+        deque.insert(0,value[1])
+    elif value[0] == "push_back":
+        deque.append(value[1])
+    elif value[0] == "pop_front":
+        print(deque.pop(0)) if deque else print(-1)
+    elif value[0] == "pop_back":
+        print(deque.pop()) if deque else print(-1)
+    elif value[0] == "front":
+        print(deque[0]) if deque else print(-1)
+    elif value[0] == "back":
+        print(deque[-1]) if deque else print(-1)
+    elif value[0] == "size":
+        print(len(deque))
+    elif value[0] == "empty":
+        print(0) if deque else print(1)
