3dcc2fcf05162a5c97becad647a762067333e3ff
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-05 10:40:02 +0900

    [Gold IV] Title: LCS 3, Time: 776 ms, Memory: 38640 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -3,20 +3,17 @@ input = sys.stdin.readline
 
 str1, str2, str3 = input().rstrip(),input().rstrip(),input().rstrip()
 R, C, K = len(str1),len(str2),len(str3)
-dp = [[[0] * (R+1) for _ in range(C+1)] for _ in range(K+1)]
+dp = [[[0] * (C+1) for _ in range(R+1)] for _ in range(K+1)]
 
 for k in range(1, K + 1):
-    for c in range(1, C + 1):
-        for r in range(1, R + 1):
+    for r in range(1, R + 1):
+        for c in range(1, C + 1):
             if str1[r-1] == str2[c-1] == str3[k-1]:
-                dp[k][c][r] = dp[k-1][c-1][r-1] + 1
+                dp[k][r][c] = dp[k-1][r-1][c-1] + 1
             else:
-                dp[k][c][r] = max(
-                    dp[k][c-1][r],
-                    dp[k-1][c-1][r],
-                    dp[k-1][c][r],
-                    dp[k][c][r-1],
-                    dp[k-1][c][r-1],
-                    dp[k][c-1][r-1])
+                dp[k][r][c] = max(
+                    dp[k-1][r][c],
+                    dp[k][r-1][c],
+                    dp[k][r][c-1],)
 
-print(dp[K][C][R])
+print(dp[-1][-1][-1])
