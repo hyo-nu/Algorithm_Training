528fd28317c3b0260aa48e8ebb14d1cb7caa028d
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-07 14:58:42 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 51260 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -6,12 +6,12 @@ N, M = map(int,input().split())
 number_dict, name_dict = {}, {}
 for i in range(1, N+1):
     poketmon = input().rstrip()
-    number_dict[str(i)] = number_dict.get(str(i),poketmon)
+    number_dict[i] = number_dict.get(i,poketmon)
     name_dict[poketmon] = name_dict.get(poketmon,i)
 
 for _ in range(M):
     problem = input().rstrip()
     if problem.isdigit():
-        print(number_dict[problem])
+        print(number_dict[int(problem)])
     else:
         print(name_dict[problem])
