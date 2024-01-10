72d7c79bcaa2e1846f7ceabf13eddd5da421f257
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-10 09:22:29 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 1292 ms, Memory: 39172 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -0,0 +1,26 @@
+import sys
+from itertools import combinations
+from collections import Counter
+input = sys.stdin.readline
+
+for test in range(int(input())):
+    N = int(input())
+    mbtikinds = []
+    for mbti, count in Counter(list(input().split())).items():
+        if count >= 3 : mbtikinds.extend([mbti] * 3)
+        elif count == 2 : mbtikinds.extend([mbti] * 2)
+        elif count == 1 : mbtikinds.extend([mbti] * 1)
+
+    min_distance = 13
+    for choice in combinations(mbtikinds,3):
+        choice = list(choice) + [choice[0]]
+        total = 0
+        for i in range(3):
+            distance = 0
+            for j in range(4):
+                if list(choice[i])[j] != list(choice[i + 1])[j]: distance += 1
+            total += distance
+            if total > min_distance: break
+        min_distance = min(min_distance, total)
+        if not min_distance: break
+    print(min_distance)
