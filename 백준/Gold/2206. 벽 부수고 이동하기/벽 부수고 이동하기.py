15975cd96de3fe66298a59080aa5afb3cdcfd1ab
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-13 13:18:58 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1572 ms, Memory: 281900 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs():
+    while Q:
+        r, c, z = Q.popleft()
+        if r == N - 1 and c == M - 1:
+            return vi[r][c][z]
+        for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < M :
+                if G[nr][nc] == 1 and z == 0 :
+                    vi[nr][nc][1] = vi[r][c][0] + 1
+                    Q.append((nr,nc,1))
+
+                elif G[nr][nc] == 0 and vi[nr][nc][z] == 0:
+                    vi[nr][nc][z] = vi[r][c][z] + 1
+                    Q.append((nr,nc,z))
+    
+    return -1
+
+N, M = map(int,input().split())
+G = [list(map(int,input().rstrip())) for _ in range(N)]
+vi = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+vi[0][0][0] = 1
+
+Q = deque([(0, 0, 0)])
+print(bfs())
