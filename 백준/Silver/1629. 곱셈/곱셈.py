851fb31713dd0dd7bd2cbe152b3c9804039f8b19
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-16 12:27:05 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,2 +1,12 @@
-A,B,C = map(int,input().split())
-print(pow(A,B,C))
+def power(num, exp, mod):
+    if exp == 0:
+        return 1
+    else :
+        x = power(num, exp//2, mod)
+        if exp % 2 == 0 :
+            return (x * x) % mod
+        else :
+            return (x * x * num) % mod
+
+A, B, C = map(int,input().split())
+print(power(A, B, C))
