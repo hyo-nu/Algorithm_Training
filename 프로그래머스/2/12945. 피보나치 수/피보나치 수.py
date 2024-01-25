6a8a04489ffd00c9b21e19ea66cbebb7a9e39399
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-25 12:27:26 +0900

    [level 2] Title: 피보나치 수, Time: 536.18 ms, Memory: 441 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,5 +1,8 @@
 def solution(n):
-    if n == 1 : return 1
-    elif n == 0 : return 0
+    dp = [0] * (n + 1)
     
-    return (solution(n-1) + solution(n-2)) % (1234567)
+    for i in range(1, n + 1):
+        if i == 1 : dp[i] = 1
+        else: dp[i] += dp[i-1] + dp[i-2]
+    
+    return dp[n] % 1234567
