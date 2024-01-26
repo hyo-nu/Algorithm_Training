dd840d0d6930ef5787973e2f312fc142ab21eeff
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-26 09:47:09 +0900

    [Gold IV] Title: Σ, Time: 120 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/13172. Σ/Σ.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def moduler (b, n, mod):
+    if n == 0:
+        return 1
+    x = moduler(b, n // 2, mod)
+    if not n % 2:
+        return (x * x) % mod
+    else :
+        return (x * x * b) % mod
+
+N = int(input())
+answer = 0
+module = 1000000007
+for _ in range(N):
+    b, a = map(int,input().split())
+    re_b = moduler(b, module-2, module)
+    answer += (a * re_b % module)
+
+print(answer%module)
