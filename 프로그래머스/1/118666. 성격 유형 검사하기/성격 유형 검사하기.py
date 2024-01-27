3a43f3051d4ec6d1f17561b52a1f38cda7831215
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-27 17:34:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.52 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -7,32 +7,53 @@
 # survey의 두번쨰 :: 동의
 
 def solution(survey, choices):
-    type_score = {}
-    character = ""
+    answer = ''
+    RTCFJMAN = [0,0,0,0,0,0,0,0]
+    str = "RTCFJMAN"
+    for i in range(len(survey)):
+        RTCFJMAN[str.index(survey[i][1])] += choices[i]-4
     
-    for idx, el in enumerate(survey):
-        disagree, agree = el[0], el[1]
         
-        # 비동의 점수 획득
-        if choices[idx] < 4:
-            score = 4 - choices[idx]
-            type_score[disagree] = type_score.get(disagree, 0) + score
+    if(RTCFJMAN[0]>=RTCFJMAN[1]): answer+= "R"
+    else: answer+="T"
+    if(RTCFJMAN[2]>=RTCFJMAN[3]): answer+= "C"
+    else: answer+="F"
+    if(RTCFJMAN[4]>=RTCFJMAN[5]): answer+= "J"
+    else: answer+="M"
+    if(RTCFJMAN[6]>=RTCFJMAN[7]): answer+= "A"
+    else: answer+="N"
+
+    return answer
+
+
+# def solution(survey, choices):
+#     type_score = {}
+#     character = ""
+    
+#     for idx, el in enumerate(survey):
+#         disagree, agree = el[0], el[1]
+        
+#         # 비동의 점수 획득
+#         if choices[idx] < 4:
+#             score = 4 - choices[idx]
+#             type_score[disagree] = type_score.get(disagree, 0) + score
             
-        # 동의 점수 획득
-        elif choices[idx] > 4:
-            score = choices[idx] - 4
-            type_score[agree] = type_score.get(agree, 0) + score
+#         # 동의 점수 획득
+#         elif choices[idx] > 4:
+#             score = choices[idx] - 4
+#             type_score[agree] = type_score.get(agree, 0) + score
         
     
-    for type1, type2 in (("R","T"),("C","F"),("J","M"),("A","N")):
-        score1 = type_score.get(type1, 0)
-        score2 = type_score.get(type2, 0)
+#     for type1, type2 in (("R","T"),("C","F"),("J","M"),("A","N")):
+#         score1 = type_score.get(type1, 0)
+#         score2 = type_score.get(type2, 0)
         
-        if score1 >= score2 : character += type1
-        elif score1 < score2 : character += type2
+#         if score1 >= score2 : character += type1
+#         elif score1 < score2 : character += type2
         
     
-    return character
+#     return character
+
 
 
 
