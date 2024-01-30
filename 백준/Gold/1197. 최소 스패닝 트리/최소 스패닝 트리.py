f8ca3934817d97501d0b51e6eb3bb4d95d1b6504
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-30 10:00:55 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 296 ms, Memory: 50600 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,26 +1,30 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 
-def find(sp):
-    if sp == rep[sp]:
-        return sp
-    rep[sp] = find(rep[sp])
-    return rep[sp]
+def find_parent(sp):
+    if sp != parent[sp]:
+        parent[sp] = find_parent(parent[sp])
+    return parent[sp]
 
-def union(sp,ep):
-    rep[find(sp)] = find(ep)
+def union(sp, ep):
+    parent[find_parent(sp)] = find_parent(ep)
 
-V, E = map(int,input().split())
-G = [list(map(int,input().split())) for _ in range(E)]
-G.sort(key=lambda x:x[2])
-rep = [i for i in range(V + 1)]
+N, M = map(int,input().split())
+parent = [i for i in range(N + 1)]
+edge = []
 
-Min = edge = 0
-for sp, ep, weight in G:
-    if find(sp) != find(ep):
-        Min += weight
-        edge += 1
+for _ in range(M):
+    sp, ep, weight = map(int,input().split())
+    edge.append((sp, ep, weight))
+
+edge.sort(key = lambda x : x[2])
+
+MST = 0
+for sp, ep, weight in edge:
+    if find_parent(sp) != find_parent(ep):
+        MST += weight
         union(sp,ep)
-        if edge == V-1:
-            break
-print(Min)
+
+print(MST)
