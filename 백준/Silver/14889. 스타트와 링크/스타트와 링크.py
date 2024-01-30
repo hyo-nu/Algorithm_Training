80ce7c999811952cf502f55fbb49402b81848fac
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-30 12:07:36 +0900

    [Silver I] Title: 스타트와 링크, Time: 312 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -2,11 +2,19 @@ import sys
 from itertools import combinations
 input = sys.stdin.readline
 
+def combi_cnt(total,choice):
+    result = 1
+    for i in range(total,choice,-1) : result *= i
+    for i in range(1,choice + 1) : result /= i
+    return int(result) // 2
+
 N = int(input())
 G = [list(map(int,input().split())) for _ in range(N)]
 
 Min = int(1e9)
-for start in combinations(range(1, N + 1), N // 2):
+case_cnt = combi_cnt(N, N//2)
+for idx, start in enumerate(combinations(range(1, N + 1), N // 2),1):
+    if idx > case_cnt : break
     start_cnt = rink_cnt = 0
 
     for r in start:
