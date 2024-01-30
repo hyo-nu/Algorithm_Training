44098d418b8e201db44e6a303ee7c8b6a0bdb76a
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-30 09:21:31 +0900

    [Gold V] Title: 집합의 표현, Time: 308 ms, Memory: 76584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,24 +1,22 @@
 import sys
-sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 
 def find_parent(sp):
     if sp != parent[sp]:
-	    parent[sp] = find_parent(parent[sp])
+        parent[sp] = find_parent(parent[sp])
     return parent[sp]
 
-def union(sp,ep):
+def union(sp, ep):
     parent[find_parent(sp)] = find_parent(ep)
 
 N, M = map(int,input().split())
 parent = [i for i in range(N + 1)]
 
 for _ in range(M):
-    Type, a, b = map(int,input().split())
-    if not Type:
-        union(a,b)
-    elif Type:
-        if find_parent(a) == find_parent(b):
-            print("YES")
-        else:
-            print("NO")
+    act, sp, ep = map(int,input().split())
+    if not act :
+        union(sp, ep)
+    else :
+        if find_parent(sp) == find_parent(ep) : print("YES")
+        else : print("NO")
