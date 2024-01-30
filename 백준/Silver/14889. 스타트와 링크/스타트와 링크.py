da1babb6d4e4e6fc96370c2385f9a39f655141e6
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-30 11:39:40 +0900

    [Silver I] Title: 스타트와 링크, Time: 564 ms, Memory: 111108 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,24 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+N = int(input())
+G = [list(map(int,input().split())) for _ in range(N)]
+
+Min = int(1e9)
+for start in combinations(range(1, N + 1), N // 2):
+    start_cnt = rink_cnt = 0
+
+    for r in start:
+        for c in start:
+            start_cnt += G[r-1][c-1]
+
+    rink = [ i for i in range(1, N + 1) if i not in start ]
+
+    for r in rink:
+        for c in rink:
+            rink_cnt += G[r-1][c-1]
+
+    Min = min(Min, abs(start_cnt - rink_cnt))
+
+print(Min)
