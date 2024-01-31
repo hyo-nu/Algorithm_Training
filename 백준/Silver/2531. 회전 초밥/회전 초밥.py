b850b1512b89bac6c679dd0e78d417a79cdb3440
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-01-31 18:09:24 +0900

    [Silver I] Title: 회전 초밥, Time: 68 ms, Memory: 32400 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -4,10 +4,28 @@ input = sys.stdin.readline
 N, d, k, c = map(int, input().split())
 belt = [int(input()) for _ in range(N)]
 belt = belt + belt[:k-1]
+kind = [0] * (d + 1)
 
-Max = 0
-for i in range(N):
-    fish = len(set(belt[i:i+k]))
-    if c not in belt[i:i+k] : fish += 1
-    Max = max(Max, fish)
+
+fishRice = 0
+for i in range(k):
+    if not kind[belt[i]]:
+        fishRice += 1
+    kind[belt[i]] += 1
+
+Max = fishRice
+if not kind[c]: Max += 1
+
+for ep in range(k, N + k - 1):
+    sp = ep - k
+    kind[belt[sp]] -= 1
+    if not kind[belt[sp]] : fishRice -= 1
+
+    if not kind[belt[ep]] :
+        fishRice += 1
+    kind[belt[ep]] += 1
+
+    if not kind[c] : Max = max(Max, fishRice + 1)
+    else : Max = max(Max, fishRice)
+
 print(Max)
