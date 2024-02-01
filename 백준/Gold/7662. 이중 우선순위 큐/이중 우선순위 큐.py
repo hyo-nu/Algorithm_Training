ff466d73e5e79245f534fda0daafb07d0a44f359
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-02-01 13:58:36 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2360 ms, Memory: 227416 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,34 +1,37 @@
 import sys
-from heapq import heappop, heappush
-
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
-Test = int(input())
+for test in range(int(input())):
+    k = int(input())
+    hmax, hmin = [], []
+    hash_max, hash_min = {}, {}
+    insert_cnt = delete_cnt = 0
 
-
-for TC in range(Test):
-    K = int(input())
-    min_h, max_h = [], []
-    vi = [0] * K
-
-    for k in range(K):
-        ID, num = map(str, input().split())
+    for _ in range(k):
+        ID, num = input().split()
         if ID == "I":
-            heappush(min_h, (int(num),k))
-            heappush(max_h, (-int(num),k))
-            vi[k] = 1
-        else:
-            if num == "-1":
-                while min_h and not vi[min_h[0][1]]: heappop(min_h)
-                if min_h: vi[heappop(min_h)[1]] = 0
-            elif num == "1":
-                while max_h and not vi[max_h[0][1]]: heappop(max_h)
-                if max_h: vi[heappop(max_h)[1]] = 0
+            insert_cnt += 1
+            heappush(hmax, -int(num))
+            heappush(hmin, int(num))
 
-        while min_h and not vi[min_h[0][1]]: heappop(min_h)
-        while max_h and not vi[max_h[0][1]]: heappop(max_h)
+        elif ID == "D" and insert_cnt > delete_cnt:
+            delete_cnt += 1
 
-    if min_h :
-        print(-max_h[0][0], min_h[0][0])
-    else:
-        print("EMPTY")
+            if hmax and num == "1":
+                Max = heappop(hmax)
+                hash_max[Max] = hash_max.get(Max, 0) + 1
+
+            elif hmin and num == "-1":
+                Min = heappop(hmin)
+                hash_min[Min] = hash_min.get(Min, 0) + 1
+
+        while hmax and -hmax[0] in hash_min and hash_min[-hmax[0]] > 0:
+            hash_min[-hmax[0]] -= 1
+            heappop(hmax)
+
+        while hmin and -hmin[0] in hash_max and hash_max[-hmin[0]] > 0:
+            hash_max[-hmin[0]] -= 1
+            heappop(hmin)
+
+    print(f"{-heappop(hmax)} {heappop(hmin)}" if hmax and hmin else "EMPTY")
