4c8c905c5a72ec5907f6dbc727ad8da8135ca424
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-02-11 15:29:43 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 764 ms, Memory: 93964 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
@@ -2,8 +2,30 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const fs = require("fs")
 let input = fs.readFileSync(filePath).toString().trim().split("\n")
 
-const [n, m, r] = input[0].split(" ").map(Number)
+class Queue{
+  head = 0
+  tail = 0
+  list = []
+  constructor(){}
+
+  append(value){
+    this.list[this.tail++] = value
+  }
+  popleft(){
+    if(this.isEmpty()){
+      return false
+    }
+    return this.list[this.head++]
+  }
+  isEmpty(){
+    return this.head === this.tail ? true : false
+  }
+}
+
+const [n, m, r] = input[0].split(" ").map((e) => Number(e))
 const G = Array.from({length: n + 1}, () => [])
+const vi = new Array(n + 1).fill(0)
+let order = 1
 
 for (let i = 1; i <= m; i++){
   const [sp,ep] = input[i].split(" ").map(Number)
@@ -12,17 +34,16 @@ for (let i = 1; i <= m; i++){
 }
 
 G.forEach((line) => line.sort((a,b) => a - b))
-const vi = new Array(n + 1).fill(0)
 
-let Q = [r]
+let Q = new Queue()
+Q.append(r)
 vi[r] = 1
 
-let order = 1
-while(Q.length) {
-  const sp = Q.shift()
+while(!Q.isEmpty()) {
+  const sp = Q.popleft()
   for (const np of G[sp]){
     if(!vi[np]) {
-      Q.push(np)
+      Q.append(np)
       vi[np] = ++order
     }
   }
