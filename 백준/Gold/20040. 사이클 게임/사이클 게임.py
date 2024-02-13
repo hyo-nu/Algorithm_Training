999f604cb9fc46fe6fb7f163d88838f991cb843b
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-02-13 18:58:44 +0900

    [Gold IV] Title: 사이클 게임, Time: 1096 ms, Memory: 105332 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,23 @@
+import sys;
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
+def find(sp):
+    if sp != parents[sp]:
+        parents[sp] = find(parents[sp])
+    return parents[sp]
+
+def union(sp, ep):
+    parents[find(sp)] = find(ep)
+
+n, m = map(int,input().split())
+parents = [i for i in range(n)]
+for order in range(1,m + 1):
+    sp, ep = map(int,input().split())
+    if find(sp) == find(ep):
+        print(order)
+        break
+    else :
+        union(sp, ep)
+else:
+    print(0)
