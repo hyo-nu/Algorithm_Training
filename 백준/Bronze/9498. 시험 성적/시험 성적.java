373968e1d42fbad9d5453d16c7b14685a79d9872
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-03-10 00:30:05 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,23 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int score = sc.nextInt();
-		if (score >= 90) {	
-			System.out.println("A");
-		}
-		else if(score >=80) {
-			System.out.println("B");
-		}
-		else if(score >=70) {
-			System.out.println("C");
-		}
-		else if(score >=60) {
-			System.out.println("D");
-		}
-		else {
-			System.out.println("F");
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (90 <= num && num <= 100) System.out.println("A");
+        else if (80 <= num && num <90) System.out.println("B");
+        else if (70 <= num && num <80) System.out.println("C");
+        else if (60 <= num && num <70) System.out.println("D");
+        else System.out.println("F");
+    }
 }
