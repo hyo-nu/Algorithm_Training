7e617d5ccaacfa6b8983da025e36cf6683975dcf
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-03-20 23:05:10 +0900

    [Bronze III] Title: 우유 축제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14720. 우유 축제/우유 축제.py
@@ -0,0 +1,9 @@
+import sys;
+input = sys.stdin.readline
+n = int(input())
+store = list(map(int, input().split()))
+cnt = 0
+for i in range(n):
+    if store[i] == cnt % 3:
+        cnt += 1
+print(cnt)
