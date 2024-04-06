16f49783433b4c3c87ad9d66ccacf65297cb2556
Hyunwoo Jeong <122512457+hyo-nu@users.noreply.github.com>
2024-04-07 08:45:37 +0900

    [Gold V] Title: 빗물, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,25 +1,18 @@
 import sys;
 input = sys.stdin.readline
 
+
 H, W = map(int,input().split())
 blocks = list(map(int,input().split()))
-G = [[0] * W for _ in range(H)]
-
-for c in range(len(blocks)):
-    for r in range(blocks[c]):
-        G[r][c] = 1
 
 result = 0
-for r in range(H):
-    for c in range(W):
-        if G[r][c] != 0: continue
-
-        start = end = 0
-        for k in range(W):
-            if G[r][k] == 0 : continue
-            if k < c : start = 1
-            else : end = 1
-
-        if start == 1 and end == 1 : result += 1
+for i in range(1, W-1):
+    left_max = max(blocks[:i])
+    right_max = max(blocks[i+1:])
+    
+    compare = min(left_max, right_max)
+    
+    if blocks[i] < compare:
+        result += compare - blocks[i]
 
 print(result)
